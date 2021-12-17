5e13db661b6036aa2b4b4ac028d6b42b21a4bd00
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-12-17 13:47:37 -0700

    Data List

---
+++ BuildingForms/Models/ProductRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildingForms.Models
+{
+    public class ProductRepository
+    {
+        private static List<Product> _Products { get; set; }
+
+        static ProductRepository()
+        {
+            _Products = new List<Product>()
+            {
+                new Product()
+                {
+                    Id = 1,
+                    Name = "Product 1",
+                    Description = "Description 1",
+                    Price = 10,
+                    isApproved = true
+                },
+                new Product()
+                {
+                    Id = 2,
+                    Name = "Product 2",
+                    Description = "Description 2",
+                    Price = 10,
+                    isApproved = true
+                },
+                new Product()
+                {
+                    Id = 2,
+                    Name = "Product 3",
+                    Description = "Description 3",
+                    Price = 20,
+                    isApproved = false
+                },
+                new Product()
+                {
+                    Id = 4,
+                    Name = "Product 4",
+                    Description = "Description 4",
+                    Price = 30,
+                    isApproved = true
+                },
+
+            };
+        }
+
+        public static List<Product> Products
+        {
+            get { return _Products; }
+        }
+
+        public static void AddProduct(Product entity)
+        {
+            _Products.Add(entity);
+        }
+    }
+}
