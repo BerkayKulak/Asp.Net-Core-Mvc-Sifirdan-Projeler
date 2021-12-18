205f610af1ad61dbe6179c41096ce8f2eaabcfd9
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-12-18 03:58:08 -0700

    DbContext

---
+++ EntityFrameworkCoreIntro/Models/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCoreIntro.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+    }
+}
