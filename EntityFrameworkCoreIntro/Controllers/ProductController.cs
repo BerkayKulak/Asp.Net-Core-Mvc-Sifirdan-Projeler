109db83ac3d90a13c5290f54e2a722fe3f3a30dd
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-12-18 03:50:53 -0700

    Entity Framework FakeRepository

---
+++ EntityFrameworkCoreIntro/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EntityFrameworkCoreIntro.Models;
+
+namespace EntityFrameworkCoreIntro.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List() => View(_productRepository.Products);
+    }
+}
