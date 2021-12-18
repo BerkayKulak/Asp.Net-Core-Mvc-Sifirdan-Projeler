9d5438e3554aceb36f5e771d4f28a881223f208c
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-12-18 11:21:56 -0700

    Creating Data

--- BlogApp.WebUI/Controllers/CategoryController.cs
+++ BlogApp.WebUI/Controllers/CategoryController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BlogApp.Data.Abstract;
+using BlogApp.Entity;
 
 namespace BlogApp.WebUI.Controllers
 {
@@ -24,6 +25,24 @@ namespace BlogApp.WebUI.Controllers
         public IActionResult List()
         {
             return View(repository.GetAll());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(repository.GetAll());
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                repository.AddCategory(category);
+                return RedirectToAction("List");
+            }
+            return View(category);
         }
     }
 }
