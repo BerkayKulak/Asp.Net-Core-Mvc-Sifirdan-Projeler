62ad48fb6eba7e3b407c24cb8e14896ed7700355
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-12-21 04:20:00 -0700

    Preparing the Project

---
+++ ModelBinding/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ModelBinding.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelBinding.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository repository;
+
+        public HomeController(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public IActionResult Index(int id) => View(repository.Get(id));
+
+
+
+    }
+}
