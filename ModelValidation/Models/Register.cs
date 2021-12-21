f66aee9a16e75f9d1fffb56fb7a677305b9a24cd
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-12-21 07:37:07 -0700

    Using Metadata

--- ModelValidation/Models/Register.cs
+++ ModelValidation/Models/Register.cs
@@ -8,10 +8,16 @@ namespace ModelValidation.Models
 {
     public class Register
     {
+        [Required]
+        [Display(Name = "User Name")]
         public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Lütfen mail giriniz")]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
         [UIHint("Date")]
+        [Required(ErrorMessage = "Lütfen Birthdate giriniz")]
         public DateTime Birthdate { get; set; }
         public bool TermsAccepted { get; set; }
     }
